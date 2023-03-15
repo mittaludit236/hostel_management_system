d27ef21ec687513cabc2926621346e31b7ec9a80
Udit Mittal <uditmittal@Udits-MacBook-Air.local>
2023-03-16 02:26:14 +0530

    4th Commit

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const http=require("http");
 const app = express();
 const _=require("lodash");
+const mongoose=require("mongoose");
 app.set('view engine', 'ejs');
-
+mongoose.connect("mongodb+srv://mittaludit236:12345@cluster0.bqkcjhs.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  retype: {
+    type: String,
+    required: true
+  }
+});
+const User=new mongoose.model("User",userSchema);
 app.get("/",function(req,res){
     res.render("home");
   });
@@ -19,6 +41,27 @@ app.get("/signin_admin",function(req,res){
 app.get("/signin_student",function(req,res){
   res.render("signin_student");
 });
+app.get("/success",function(req,res){
+  res.render("success");
+});
+app.get("/failure",function(req,res){
+  res.render("failure");
+});
+app.post("/signup",function(req,res){
+  const newUser=new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    retype: req.body.password1
+  });
+  if(req.body.password == req.body.password1)
+  {
+  newUser.save();
+  res.redirect("/success");
+  }
+  else
+  res.redirect("/failure");
+})
 
 
 
