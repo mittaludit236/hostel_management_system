10440586f0f62a2a542f449cff0fdc88db58f8af
Udit Mittal <uditmittal@Udits-MacBook-Air.local>
2024-03-29 17:35:08 +0530

    to resolve conflicts

--- utilities/date.js
+++
@@ -1,9 +0,0 @@
-exports.getDate=function()
-{
-   var options={
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-   };
-   return new Date().toLocaleDateString("en-US",options);
-};
--- utilities/help.js
+++
@@ -1,7 +0,0 @@
-exports={};
-const jwt=require("jsonwebtoken");
-exports.getToken=(email,user)=>{
-    const token=jwt.sign({identifier: user._id},"thisIsSupposedToBeSecret");
-    return token;
-};
-module.exports=exports;
